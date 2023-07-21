659133b262c40f6267e6001fca867a4a6e186390
Dylan Silverglate <dylansilverglate@Dylans-Air.lan>
2023-07-21 14:19:40 -0700

    settings changes

--- dnd_charactersheet/settings.py
+++ dnd_charactersheet/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main_app/static')]
 
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:
